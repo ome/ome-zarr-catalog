ddfa12f6d2c5f849b3e0256cb129e7cff3fb7b58
William Moore <w.moore@dundee.ac.uk>
2023-03-10 16:21:27 +0000

    Use vizarr_logo.png as template for dynamic viewer logos

--- src/OpenWith.jsx
+++ src/OpenWith.jsx
@@ -1,6 +1,8 @@
 import React from "react";
 
 import openwithJson from "../public/openwith.json";
+// use static import of vizarr_logo.png to get base URL for other logos
+import vizarr_logo from "/vizarr_logo.png";
 
 export default function OpenWith({ source }) {
   let viewers = openwithJson.viewers;
@@ -9,7 +11,7 @@ export default function OpenWith({ source }) {
     <React.Fragment>
       {viewers.map((viewer) => (
         <a key={viewer.name} target="_blank" href={viewer.href + source} title={"Open with " + viewer.name}>
-          <img className="viewer_icon" src={viewer.logo} />
+          <img className="viewer_icon" src={vizarr_logo.replace("/vizarr_logo.png", viewer.logo)} />
         </a>
       ))}
     </React.Fragment>
